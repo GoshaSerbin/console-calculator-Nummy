f5981c486c63fd2c0e54e15b3ac04168fef226fe
GoshaSerbin <gosha_serbin00@mail.ru>
2023-07-03 23:44:12 +0300

    added gmock test

--- tests/BasicCalculator.cpp
+++ tests/BasicCalculator.cpp
@@ -1,17 +1,35 @@
 #include "BasicCalculator.hpp"
+#include "CalculatableTree.hpp"
+#include "ExpressionTokenizer.hpp"
+#include "ExpressionValidator.hpp"
+#include "TokensManager.hpp"
+#include "VariablesManager.hpp"
 
 #include <gtest/gtest.h>
-#include "sstream"
+#include <sstream>
 
 namespace Nummy {
 
     class BasicCalculatorTest : public testing::Test {
      protected:
         void SetUp() override {
-            // calculator = std::make_unique<BasicCalculator>(in, out, std::move(validator), std::move(tokenizer), std::move(tree),
-            //                                                std::move(tokensManager), std::move(variablesManager));
+            validator = std::make_unique<ExpressionValidator>();
+            tokenizer = std::make_unique<ExpressionTokenizer>();
+
+            tokensManager = std::make_unique<TokensManager>();
+            variablesManager = std::make_unique<VariablesManager>(*tokensManager);
+            tree = std::make_unique<CalculatableTree>(*variablesManager);
+            calculator = std::make_unique<BasicCalculator>(in, out, std::move(validator), std::move(tokenizer), std::move(tree),
+                                                           std::move(tokensManager), std::move(variablesManager));
         }
         void TearDown() override {}
+
+        std::unique_ptr<IExpressionValidator> validator;
+        std::unique_ptr<IExpressionTokenizer> tokenizer;
+
+        std::unique_ptr<ITokensManager> tokensManager;
+        std::unique_ptr<IVariablesManager> variablesManager;
+        std::unique_ptr<ICalculatableTree> tree;
 
         std::stringstream in;
         std::stringstream out;
