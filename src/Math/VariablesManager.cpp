8514d6fa1cedc91c394b9260e89a26b31c67b27f
GoshaSerbin <gosha_serbin00@mail.ru>
2023-07-05 13:08:04 +0300

    added variable manager check before adding token

--- src/Math/VariablesManager.cpp
+++ src/Math/VariablesManager.cpp
@@ -5,8 +5,10 @@ namespace Nummy {
     VariablesManager::VariablesManager(ITokensManager& tokensManager) : m_tokensManager(tokensManager) {}
 
     auto VariablesManager::setVariable(const std::string& name, double value) -> void {
+        if (!m_values.contains(name)) {
+            m_tokensManager.addToken(Token{name, TokenType::Variable});
+        }
         m_values[name] = value;
-        m_tokensManager.addToken(Token{name, TokenType::Variable});
     }
 
     auto VariablesManager::getVariable(const std::string& name) -> double {
